2bcc9c4edd048a0da7ff1de3433d0ed8e0677184
Ilnarey <ilnaragaliullina16@gmail.com>
2025-03-02 17:32:26 +0500

    Update app.tsx

--- my-rent-service/src/components/app/app.tsx
+++ my-rent-service/src/components/app/app.tsx
@@ -7,21 +7,25 @@ import OfferPage from "../../pages/offer-page/offer-page";
 import { BrowserRouter as  BrowserRouter , Routes, Route,} from "react-router-dom";
 import { AppRoute, AuthorizationStatus } from "../../const.ts";
 import { PrivateRoute } from "../private-route/private-route.tsx";
+import { FullOffer, OffersList } from "../../types/offer.ts";
+
 type AppMainPageProps={
     rentalOffersCount: number;
+    offersList : OffersList[];
+    offers: FullOffer[];
 }
 
 
-function App({rentalOffersCount}: AppMainPageProps): JSX.Element{
+function App({rentalOffersCount, offers,offersList}: AppMainPageProps): JSX.Element{
   return(
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<MainPage rentalOffersCount={rentalOffersCount} />} />
+        <Route path={AppRoute.Main} element={<MainPage rentalOffersCount={rentalOffersCount} offersList={offersList} />} />
         <Route path={AppRoute.Login} element={<LoginPage/>} />
-        <Route path={AppRoute.Offer} element={<OfferPage/>} />
+        <Route path={`${AppRoute.Offer}/:id`} element={<OfferPage offers={offers}/>} />
         <Route
             path={ AppRoute.Favorites }
-            element={<PrivateRoute authorizationStatus={ AuthorizationStatus.NoAuth }>
+            element={<PrivateRoute authorizationStatus={ AuthorizationStatus.Auth }>
               <FavoritesPage />
               </PrivateRoute>
             }/>
