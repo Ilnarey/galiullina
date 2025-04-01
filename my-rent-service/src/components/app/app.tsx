9b4215afdcbca9b12a2e77545c1b2ea3fb1272e4
Ilnarey <ilnaragaliullina16@gmail.com>
2025-04-02 02:02:17 +0500

    lab5-6

--- my-rent-service/src/components/app/app.tsx
+++ my-rent-service/src/components/app/app.tsx
@@ -4,32 +4,38 @@ import LoginPage from "../../pages/login-page/login-page";
 import FavoritesPage from "../../pages/favorites-page/favorites-page";
 import ErrorPage from "../../pages/error-page/error-page";
 import OfferPage from "../../pages/offer-page/offer-page";
-import { BrowserRouter as  BrowserRouter , Routes, Route,} from "react-router-dom";
-import { AppRoute, AuthorizationStatus } from "../../const.ts";
-import { PrivateRoute } from "../private-route/private-route.tsx";
-import { FullOffer, OffersList } from "../../types/offer.ts";
+import { BrowserRouter as BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppRoute} from "../../const";
+import { PrivateRoute } from "../private-route/private-route";
+import { AutorizationStatus } from "../../const";
+import { FullOffer, OffersList } from "../../types/offer";
+import { Review } from "../../types/review";
 
 type AppMainPageProps={
     rentalOffersCount: number;
-    offersList : OffersList[];
+    offersList : OffersList[]
     offers: FullOffer[];
+    reviewsList : Review[];
+    reviewsOffersCount: number
 }
 
 
-function App({rentalOffersCount, offers,offersList}: AppMainPageProps): JSX.Element{
+
+function App({ offers,offersList,reviewsList,reviewsOffersCount}: AppMainPageProps): JSX.Element{
   return(
     <BrowserRouter>
-      <Routes>
-        <Route path={AppRoute.Main} element={<MainPage rentalOffersCount={rentalOffersCount} offersList={offersList} />} />
+      <Routes>  
+        <Route path={AppRoute.Main} element={<MainPage />} />
         <Route path={AppRoute.Login} element={<LoginPage/>} />
-        <Route path={`${AppRoute.Offer}/:id`} element={<OfferPage offers={offers}/>} />
-        <Route
-            path={ AppRoute.Favorites }
-            element={<PrivateRoute authorizationStatus={ AuthorizationStatus.Auth }>
-              <FavoritesPage />
-              </PrivateRoute>
-            }/>
-
+        <Route path={`${AppRoute.Offer}/:id`} element={<OfferPage  offers={offers} reviewsList={reviewsList} offersList={ offersList } reviewsOffersCount={reviewsOffersCount}/>} />
+        <Route path={AppRoute.Favorites} 
+          element={
+            <PrivateRoute
+              authorizationStatus={AutorizationStatus.Auth}>
+                <FavoritesPage offersList={ offersList }/>
+            </PrivateRoute>
+            } 
+          />
         <Route path="*" element={<ErrorPage/>} />
       </Routes>
     </BrowserRouter>
--- my-rent-service/src/components/cities-card/cities-card.tsx
+++ my-rent-service/src/components/cities-card/cities-card.tsx
@@ -42,7 +42,7 @@ function CitiesCard({ id, title, type, price, previewImage, isPremium, rating}: 
           </div>
           <div className="place-card__rating rating">
             <div className="place-card__stars rating__stars">
-              <span style={{width: "80%"}}></span>
+              <span style={{width: `${(rating / 5) * 100}%` }}></span>
               <span className="visually-hidden"> {rating} </span>
             </div>
           </div>
